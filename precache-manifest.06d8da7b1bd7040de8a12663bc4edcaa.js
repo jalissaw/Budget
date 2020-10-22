9830b49c2302966bfd0b1624a35727804f8408f0
Jalissa Williams <jalissa_williams@yahoo.com>
2020-10-22 12:14:07 -0400

    Updates

---
+++ precache-manifest.06d8da7b1bd7040de8a12663bc4edcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46849f6808f276d9921255c633129624",
+    "url": "/Budget/index.html"
+  },
+  {
+    "revision": "88f44d9c69add5068310",
+    "url": "/Budget/static/css/main.443a06a0.chunk.css"
+  },
+  {
+    "revision": "6f53be825026d3b46694",
+    "url": "/Budget/static/js/2.6a63ada0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget/static/js/2.6a63ada0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f44d9c69add5068310",
+    "url": "/Budget/static/js/main.20080b35.chunk.js"
+  },
+  {
+    "revision": "9a78eecff82d07673d84",
+    "url": "/Budget/static/js/runtime-main.ca0d937e.js"
+  }
+]);
